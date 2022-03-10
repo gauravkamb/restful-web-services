a87e731beeafb2f43897519a81e109ccaf312119
Gaurav Kamble <gauravkamble909@gmail.com>
2022-03-10 11:50:36 +0530

    implement static filtering

--- src/main/java/com/gaurav/restfulwebservices/user/User.java
+++ src/main/java/com/gaurav/restfulwebservices/user/User.java
@@ -4,16 +4,20 @@ import java.util.Date;
 
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonIgnoreProperties({"name", "id"})
 public class User {
 	
 	private Integer id;
 	@Size(min = 2, message = "min size 2 required")
 	private String name;
+	//@JsonIgnore
 	private Date birthDate;
 
 }
